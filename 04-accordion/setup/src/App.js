f787807617814058bca08ed011b36b29e7b3081a
zocc-n <znanic@yahoo.com>
2023-06-19 10:16:47 +0200

    04 - Accordion

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
+
 function App() {
-  return <h2>accordion project setup</h2>;
+
+  return (
+    <main>
+      <div className="container">
+        <h3>questions</h3>
+        <section className="info">
+          {data.map(question => {
+            return (<SingleQuestion 
+              key={question.id} 
+              id={question.id}
+              title={question.title}
+              info={question.info}
+            />)
+          })
+          }
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
-};
+
+const Question = ({title, info}) => {
+
+  const [show, setShow] = useState(false)
+  
+  const toggleShow = () => {
+    setShow(!show)
+  }
+
+  return (
+    <article className='question'>
+      <header>
+       <h4>{title}</h4>
+      
+      
+        <button className='btn' onClick={toggleShow}>
+          {
+            show
+              ? <AiOutlineMinus />
+              : <AiOutlinePlus />
+          }
+        </button>
+      </header>
+
+      {
+        show
+          ? <p>{info}</p>
+          : null
+      }
+
+    </article>
+  )
+}
 
 export default Question;
