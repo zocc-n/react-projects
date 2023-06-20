d04e73e0e9f72acf0c7ed657bfafe5464996d0c0
zocc-n <znanic@yahoo.com>
2023-06-20 11:37:25 +0200

    06 - Tabs

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,10 +1,73 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect( () => {
+    fetchJobs();
+  }, [])
+
+
+
+  if(loading){
+    return (
+      <section className='section loading'>
+        <h1>loading</h1>
+      </section>
+    )
+  }
+
+  const {company, dates, duties, title} = jobs[value]
+  //the destructuring must be after the loading. if data is not loaded yet it would destructure an empty array which causes an error
+
+  return (
+    <section className='section'>
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {
+            jobs.map((item,index) => {
+              return (
+              <button 
+                key={item.id} 
+                onClick={() => setValue(index)}
+                //ako je index jednak value znaci da je taj tab/button trenutno aktivan i onda proprima active-btn klasu
+                className={`job-btn ${index === value && 'active-btn'}`}>
+                  {item.company}
+              </button>
+              )
+            })
+          }
+        </div>
+      <article className='job-info'>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty,i) => {
+          return <div key={i} className="job-desc">
+            <FaAngleDoubleRight className='job-icon' />
+            <p>{duty}</p>
+          </div>
+        })}
+      </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
