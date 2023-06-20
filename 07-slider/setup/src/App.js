a7af625368f8b60d5295ef799cf71451cb007623
zocc-n <znanic@yahoo.com>
2023-06-20 18:54:54 +0200

    07 - Slider

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -2,8 +2,65 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
-function App() {
-  return <h2>slider project setup</h2>;
-}
 
+function App() {
+  
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if(index < 0){
+      setIndex(lastIndex)
+    }
+    if(index > lastIndex){
+      setIndex(0)
+    }
+  }, [index,people])
+
+  useEffect(() => {
+    let slider = setInterval(()=>{
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider)
+  }, [index])
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person,i) => {
+          const {id, image, title, name, quote} = person;
+          let position = 'nextSlide';
+          if(i === index){
+            position = 'activeSlide';
+          }
+          if(i === index-1 || (i === 0 && i === people.length-1)){
+            position = 'lastSlide';
+          }
+
+          return (
+            <article key={id} className={position}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          )
+        })}
+        <button className='prev' onClick={()=>setIndex(index-1)}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={()=>setIndex(index+1)}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  )
+}
 export default App;
