606f8c4ce5cc40d3c53bae1dab7bbcf79d93e15b
zocc-n <znanic@yahoo.com>
2023-06-22 21:46:58 +0200

    09 - Color Generator

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,45 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
-}
+  
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
-export default App
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      try{
+        let colors = new Values(color).all(10)
+        setList(colors)
+      }
+      catch(error){
+        setError(true);
+        console.log(error);
+      }
+  }
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            placeholder='#f15025'
+            value={color} 
+            onChange={e => setColor(e.target.value)}
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list foes here</h4>
+        {list.map((color,index) => {
+          return <SingleColor key={index} {...color} index={index} />
+        })}
+      </section>
+    </>
+  )
+}
+export default App;
